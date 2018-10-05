4cc35e2111ec38fe77e9198470c0eb2fcc23b71f
pw257 <42645110+pw257@users.noreply.github.com>
2018-10-05 18:28:47 -0500

    Individual Programs

---
+++ Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatrickW_Pets
+{
+    public class Pet
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public double Weight { get; set; }
+
+        public Pet(string type, string name, string owner, double weight)
+        {
+            Name = name;
+            Type = type;
+            Owner = owner;
+            Weight = weight;
+        }
+
+        public object GetTag()
+        {
+            Console.WriteLine("If lost, please call Jose");
+            return;
+        }
+        
+    }
+}
