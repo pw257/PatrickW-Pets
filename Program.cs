8625df9226cc3d685814094f3a3b7e68a9e6008f
pw257 <42645110+pw257@users.noreply.github.com>
2018-10-05 18:27:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatrickW_Pets
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pet pet1 = new Pet("dog", "Shadow", "Jose", 42.6);
+
+            Console.WriteLine("Name: " + pet1.Name);
+            Console.WriteLine("Weight: " + pet1.Weight);
+            Console.WriteLine(pet1.GetTag());
+
+            Console.WriteLine ("");
+
+            /* Dog dog1 = new Dog("Daisy", "Kent", 23.4);
+
+            Console.WriteLine("Name: " + dog1.name);
+            Console.WriteLine("Weight: " + dog1.weight);
+            Console.WriteLine(dog1.getTag());
+            Console.WriteLine(dog1.bark(4));
+
+            Console.WriteLine ("");
+
+            Cat cat1 = new Cat("Simba", "Maria", 5.2);
+
+            Console.WriteLine("Name: " + cat1.name);
+            Console.WriteLine("Weight: " + cat1.weight);
+            Console.WriteLine(cat1.getTag());
+            Console.WriteLine(cat1.meow(3));
+
+            Console.WriteLine(""); */
+
+        }
+    }
+}
